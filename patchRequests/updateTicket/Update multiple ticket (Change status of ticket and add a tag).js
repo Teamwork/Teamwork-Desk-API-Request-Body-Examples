62d6edabffaf694636e5c63840589dd200a7203d
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2024-04-12 19:52:25 +0100

    Update Update multiple ticket (Change status of ticket and add a tag).js

--- patchRequests/updateTicket/Update multiple ticket (Change status of ticket and add a tag).js
+++ patchRequests/updateTicket/Update multiple ticket (Change status of ticket and add a tag).js
@@ -3,20 +3,23 @@ APIKEY = "apiKeyHere"
 siteName = "yourSiteName"
 ticketStatus = ticketStatusHere// 0 = Active, 3 - Waiting on customer, 4 = On-hold, 5 = Solved, 6 = Closed, 7 = Spam (For other statuses use https://apidocs.teamwork.com/docs/desk/v2/ticket-statuses/get-v2-ticketstatuses-json)
 tagId = tagIdHere//For tag ids use https://apidocs.teamwork.com/docs/desk/v2/tags/get-v2-tags-json
-ticketId = "ticketIdHere"
+ticketId(s) = "ticketIdsHere"// comman separate ticket ids if there is more than one ticket
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Authorization", "Bearer "+APIKEY);
 
 const raw = JSON.stringify({
-  "ticket": {
-    "status": {
-      "id": ticketStatus
+  "filter_args": {
+    "filter": {
+      "id": {
+        "$in": [
+         ticketId(s)
+        ]
+      }
     },
-    "tags": [
-        {
-            "id": tagId
-        }
-    ]
+    "bulkDryExecute": false
+  },
+  "status": {
+    "id": ticketStatus
   }
 });
 
@@ -27,7 +30,7 @@ const requestOptions = {
   redirect: "follow"
 };
 
-fetch("https://"+siteName+".teamwork.com/desk/api/v2/tickets/"+ticketId+".json", requestOptions)
+fetch("https://"+siteName+".teamwork.com/desk/api/v2/tickets/update.json", requestOptions)
   .then((response) => response.text())
   .then((result) => console.log(result))
   .catch((error) => console.error(error));
