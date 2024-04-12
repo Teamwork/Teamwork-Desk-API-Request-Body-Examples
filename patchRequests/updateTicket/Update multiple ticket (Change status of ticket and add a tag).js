39ec44318b58ce8a62a3fa2f768793f1544945ec
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2024-04-12 10:36:26 +0100

    Create Update multiple ticket (Change status of ticket and add a tag).js

---
+++ patchRequests/updateTicket/Update multiple ticket (Change status of ticket and add a tag).js
@@ -0,0 +1,33 @@
+const myHeaders = new Headers();
+APIKEY = "apiKeyHere"
+siteName = "yourSiteName"
+ticketStatus = ticketStatusHere// 0 = Active, 3 - Waiting on customer, 4 = On-hold, 5 = Solved, 6 = Closed, 7 = Spam (For other statuses use https://apidocs.teamwork.com/docs/desk/v2/ticket-statuses/get-v2-ticketstatuses-json)
+tagId = tagIdHere//For tag ids use https://apidocs.teamwork.com/docs/desk/v2/tags/get-v2-tags-json
+ticketId = "ticketIdHere"
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Bearer "+APIKEY);
+
+const raw = JSON.stringify({
+  "ticket": {
+    "status": {
+      "id": ticketStatus
+    },
+    "tags": [
+        {
+            "id": tagId
+        }
+    ]
+  }
+});
+
+const requestOptions = {
+  method: "PATCH",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("https://"+siteName+".teamwork.com/desk/api/v2/tickets/"+ticketId+".json", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
