cfd2f946af6bc7bf89cf3d40f4451bfda9aa895b
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2024-04-12 20:00:52 +0100

    Update Update multiple ticket (Change status of ticket and add a tag).js

--- patchRequests/updateTicket/Update multiple ticket (Change status of ticket and add a tag).js
+++ patchRequests/updateTicket/Update multiple ticket (Change status of ticket and add a tag).js
@@ -3,7 +3,7 @@ APIKEY = "apiKeyHere"
 siteName = "yourSiteName"
 ticketStatus = ticketStatusHere// 0 = Active, 3 - Waiting on customer, 4 = On-hold, 5 = Solved, 6 = Closed, 7 = Spam (For other statuses use https://apidocs.teamwork.com/docs/desk/v2/ticket-statuses/get-v2-ticketstatuses-json)
 tagId = tagIdHere//For tag ids use https://apidocs.teamwork.com/docs/desk/v2/tags/get-v2-tags-json
-ticketId(s) = "ticketIdsHere"// comman separate ticket ids if there is more than one ticket
+ticketIds = "ticketIdsHere"// comman separate ticket ids if there is more than one ticket
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Authorization", "Bearer "+APIKEY);
 
@@ -12,7 +12,7 @@ const raw = JSON.stringify({
     "filter": {
       "id": {
         "$in": [
-         ticketId(s)
+         ticketIds
         ]
       }
     },
@@ -20,7 +20,12 @@ const raw = JSON.stringify({
   },
   "status": {
     "id": ticketStatus
-  }
+  },
+    "tags": [
+        {
+            "id": tagId
+        }
+    ]
 });
 
 const requestOptions = {
