7de399d25c9d69c7e450e1cd29ae40c4cbd1c48e
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2024-08-20 11:15:11 +0100

    Update Reply to a ticket(html).js

--- postRequests/replyToTicket/Reply to a ticket(html).js
+++ postRequests/replyToTicket/Reply to a ticket(html).js
@@ -14,7 +14,7 @@ const raw = JSON.stringify({
   "files": [],
   "editMethod": "html",
   "message": "\n<div>Hey Peter,<br></div><div class=\"\"><br></div><div class=\"\">Yes, it is possible to reply to a Teamwork Desk ticket with the&nbsp;<a href=\"https://apidocs.teamwork.com/docs/desk/v2/messages/post-v2-tickets-ticket-id-messages-json\" target=\"_blank\">reply to a ticket</a> endpoint.&nbsp;<br></div><div class=\"\"><br></div><div class=\"\">If you have any further questions, please get back to me.<br></div><div class=\"\"><br></div><div class=\"\">Marc</div>",
-  "threadType": "message",
+  "threadType": "message",// "message" or "note" are the two options
   "status": {
     "id": 3
   },//Waiting on customer, 1 - Active, 4 - On-Hold, 5 - Solved, 6 - Closed, 7 - Spam
