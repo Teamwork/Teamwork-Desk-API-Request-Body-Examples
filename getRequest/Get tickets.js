7b12813e01e4bc67ff8c77f44e78c89a42c2e683
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2025-03-15 01:01:14 +0000

    Update Get tickets.js

--- getRequest/Get tickets.js
+++ getRequest/Get tickets.js
@@ -1,3 +1,5 @@
+// This code sample will make a reqest to the tickets endpoint and loop through tickets until pages have exausted
+// There is addional logic to monitor the response headers for rate limit numbers
 const myHeaders = new Headers();
 APIKEY = "apiKeyHere"
 siteName = "yourSiteName"
@@ -12,19 +14,31 @@ const requestOptions = {
   headers: myHeaders
 };
 
-async function fetchTime() {
+async function fetchTickets() {
 
-        let ticketsUrl = "https://" + siteName + ".teamwork.com/desk/api/v2/tickets.json?page=" + page + "&pageSize=100"
-        const response = await fetch(ticketsUrl, requestOptions)
-        let data = await response.json()
-        console.log(data)
-        console.log("Page: " + data.pagination.pages)
-        if (page < data) {
-            page++
-            loop = true;  
-        } else {
-            loop = false;
-        }
+    let ticketsUrl = "https://" + siteName + ".teamwork.com/desk/api/v2/tickets.json?page=" + page + "&pageSize=100"
+    const response = await fetch(ticketsUrl, requestOptions)
+    let data = await response.json()
+    console.log(data)
+    console.log("Page: " + data.pagination.pages)
+    if (page < data) {
+        page++
+        loop = true;
+    } else {
+        loop = false;
+    }
+    console.log(`\nHeaders below ----------------`)
+    console.log(response.headers)
+    console.log(`\nPagination information -------`)
+    console.log(`Total records for request: ${data.pagination.records}`)
+    console.log(`Total pages: ${data.pagination.pages}`)
+    console.log(`Current page: ${data.pagination.page}`)
+    console.log(`Has morePages: ${data.pagination.hasMorePages}`)
+    console.log(`\nRate limit -------------------`)
+    console.log(`x-ratelimit-limit: ${response.headers.get("x-rate-limit-limit")}`)
+    console.log(`x-ratelimit-remaining: ${response.headers.get("X-Rate-Limit-Remaining")}`)
+    console.log(`x-ratelimit-reset: ${response.headers.get("x-rate-limit-reset")}\n`)
+
 }
-    
-fetchTime();
+
+fetchTickets();
