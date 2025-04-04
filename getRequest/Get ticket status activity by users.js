5a408006a4b3727595ca1e5363617054ec44f7e9
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2025-04-04 15:36:18 +0100

    Create Get ticket status activity by users.js

---
+++ getRequest/Get ticket status activity by users.js
@@ -0,0 +1,50 @@
+const myHeaders = new Headers();
+APIKEY = "apiKeyHere"
+siteName = "yourSiteName"
+ticketId = "ticketIdHere"
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Bearer " + APIKEY);
+
+let page = 1
+
+const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow"
+};
+
+async function fetchTicketActivities() {
+    let ticketStatusesUrl = `https:///${siteName}.teamwork.com/desk/api/v2/ticketstatuses.json`;
+    const ticketStatusesResponse = await fetch(ticketStatusesUrl, requestOptions)
+    let ticketStatusesData = await ticketStatusesResponse.json()
+    let ticketStatuses = ticketStatusesData.ticketstatuses
+  
+    let ticketsUrl = `https://${siteName}.teamwork.com/desk/api/v2/tickets/${ticketId}.json?includes=ticketactivities,users,ticketstatuses`;
+    const response = await fetch(ticketsUrl, requestOptions)
+    let data = await response.json()
+  
+    let activities = data.included.ticketactivities
+    let users = data.included.users
+
+    activities.forEach((activity, index) => {
+        if (activity.eventType == "status" && activity.createdBy.type == "users") {
+            console.log(`Activity Id: ${activity.id}`);
+            console.log(`Activity Type: ${activity.eventType}`);
+            console.log(`Activity Date: ${activity.createdAt}`);
+            console.log(`Status Icon: ${activity.icon}`);
+            ticketStatuses.forEach((status, index) => {
+                if (`${activity.status.id}` == status.id) {
+                    console.log(`Status Name: ${status.name}`);
+                }
+            });
+            console.log(`User Id: ${activity.createdBy.id}`);
+            users.forEach((user, index) => {
+                if (`${activity.createdBy.id}` == user.id) {
+                    console.log(`User Name: ${user.firstName} ${user.lastName}\n`);
+                }
+            });
+        }
+    });
+}
+
+fetchTicketActivities();
