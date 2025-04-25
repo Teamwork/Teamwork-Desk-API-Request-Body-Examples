a1b6d8c3beeff010fd938a77bfd9f9f8e1d0ea47
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2025-04-25 13:01:57 +0100

    Create Create an inbox (Basic setup).js

---
+++ postRequests/inboxes/Create an inbox (Basic setup).js
@@ -0,0 +1,41 @@
+// this code sample will create an inbox with basicx information
+// Inbox name, inbox email address and user access based on the user id
+const myHeaders = new Headers();
+APIKEY = "apiKeyHere"
+siteName = "yourSiteName"
+inboxName = "inboxNameHere"
+inboxLocalPart = "apiinbox"// the local part is a code name created based on the inbox name with out spaces and capatilization
+inboxEmail = "inboxEmailHere"
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", `Bearer ${APIKEY}`);
+
+const raw = JSON.stringify({
+  "inbox": {
+    "name": inboxName,
+    "localPart": inboxLocalPart,
+    "email": inboxEmail,
+    "users": [
+      {
+        "id": 238860
+      },
+      {
+        "id": 247628
+      },
+      {
+        "id": 250388
+      }
+    ]
+  }
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch(`https://{siteName}.teamwork.com/desk/api/v2/inboxes.json`, requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
